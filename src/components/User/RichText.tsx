7d3cbd584043f4ff1a6664c1c78993170e2c716e
Bogdan Carmanus <carmanusbogdan@gmail.com>
2024-11-19 11:46:01 +0200

    added heading buttons to richtext

--- src/components/User/RichText.tsx
+++ src/components/User/RichText.tsx
@@ -155,6 +155,54 @@ export const RichText = ({ content, layout, margin, padding, size }: any) => {
                         >
                             Bullet List
                         </button>
+                        <button
+                            onClick={() =>
+                                editor
+                                    .chain()
+                                    .focus()
+                                    .toggleHeading({ level: 1 })
+                                    .run()
+                            }
+                            className={
+                                editor.isActive('heading', { level: 1 })
+                                    ? 'is-active'
+                                    : ''
+                            }
+                        >
+                            H1
+                        </button>
+                        <button
+                            onClick={() =>
+                                editor
+                                    .chain()
+                                    .focus()
+                                    .toggleHeading({ level: 2 })
+                                    .run()
+                            }
+                            className={
+                                editor.isActive('heading', { level: 2 })
+                                    ? 'is-active'
+                                    : ''
+                            }
+                        >
+                            H2
+                        </button>
+                        <button
+                            onClick={() =>
+                                editor
+                                    .chain()
+                                    .focus()
+                                    .toggleHeading({ level: 3 })
+                                    .run()
+                            }
+                            className={
+                                editor.isActive('heading', { level: 3 })
+                                    ? 'is-active'
+                                    : ''
+                            }
+                        >
+                            H3
+                        </button>
                     </div>
                 </BubbleMenu>
             )}
